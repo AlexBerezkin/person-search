3b3883af533e70e347aa218db661d8da7580d54e
AlexBerezkin <mail.berezkin@gmail.com>
2019-09-30 22:00:51 -0700

    Updates

---
+++ precache-manifest.5c5ace38b6e139662d180db42acd7c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282a65a88e5360e61826565cffb44648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23456386616d2fe3609a",
+    "url": "/static/css/main.27b7b361.chunk.css"
+  },
+  {
+    "revision": "0ebfc92e681aa483dc6d",
+    "url": "/static/js/2.f55bae4f.chunk.js"
+  },
+  {
+    "revision": "23456386616d2fe3609a",
+    "url": "/static/js/main.b4636942.chunk.js"
+  },
+  {
+    "revision": "3d5d22ee91229fd356af",
+    "url": "/static/js/runtime-main.721448b8.js"
+  }
+]);
