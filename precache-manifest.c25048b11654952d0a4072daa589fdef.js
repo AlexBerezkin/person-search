e950c5a0de812c2519fd61f4fc1375462cf41dc3
AlexBerezkin <mail.berezkin@gmail.com>
2019-09-30 22:03:47 -0700

    Updates

---
+++ precache-manifest.c25048b11654952d0a4072daa589fdef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de050debadbeb383d4509e9ede5a76b",
+    "url": "/AlexBerezkin/person-search/index.html"
+  },
+  {
+    "revision": "940da03480fe218279fb",
+    "url": "/AlexBerezkin/person-search/static/css/main.287cc33b.chunk.css"
+  },
+  {
+    "revision": "0ebfc92e681aa483dc6d",
+    "url": "/AlexBerezkin/person-search/static/js/2.f55bae4f.chunk.js"
+  },
+  {
+    "revision": "940da03480fe218279fb",
+    "url": "/AlexBerezkin/person-search/static/js/main.5e1f3835.chunk.js"
+  },
+  {
+    "revision": "13773c93ce296bf14725",
+    "url": "/AlexBerezkin/person-search/static/js/runtime-main.30ee1bc3.js"
+  }
+]);
